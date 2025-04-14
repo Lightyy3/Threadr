a015acc11b20e503ce985d09928770fa843a03cc
alex.dinu98@yahoo.com <alex.dinu98@yahoo.com>
2025-04-14 10:59:44 +0300

    solved errors10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      "@typescript-eslint/no-unused-expressions": "off",
       "no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
     },
   },
 ];
